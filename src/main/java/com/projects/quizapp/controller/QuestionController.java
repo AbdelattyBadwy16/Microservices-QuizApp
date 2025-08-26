7c374880af21f818f5890fca111b81da98ca9d93
abdelattybadwy16 <abdelattybadwy166@gmail.com>
2025-08-26 14:04:30 +0300

    feat:add question controller and service

---
+++ src/main/java/com/projects/quizapp/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.projects.quizapp.controller;
+
+
+import com.projects.quizapp.Question;
+import com.projects.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestion(){
+        return questionService.getAllQuestions();
+    }
+}
